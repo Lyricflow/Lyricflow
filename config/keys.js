748858a996ba5f1bff897e4832e7c4db31e15e7b
Henry Jetmundsen <henryjetmundsen@gmail.com>
2018-09-06 22:16:30 -0400

    Linked the firebase db to our project. Cleaned up the genius api call and parameters. The find song intent isn't working and I believe that it is because google assistant isn't passing any information to the intent. We need to find a way to have assistant pass the entire user input string to the intent

--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.export = {
-	genius: "wpK1z0Fh9Q1cs38h1mdEfPxja6mLQv_KBEfblNSd87FjvlGu-qcJNNoJ56CdC4QU",
-}
---
+++ functions/config/requestOptions.js
@@ -0,0 +1,13 @@
+const keys = require("./keys");
+
+module.exports = {
+	genius: {
+		url: "api.genius.com/search",
+		headers: {
+			Authorization: keys.genius
+		},
+		qs: {
+			q: ""
+		}
+	}
+}
