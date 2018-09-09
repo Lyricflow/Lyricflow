4b48905f0c7a518c06eb6a92d109f9d074e2c268
Henry Jetmundsen <henryjetmundsen@gmail.com>
2018-09-09 15:59:08 -0400

    Added eslint to keep everyone on the same styling. No progress in attempting to get the fallback intent to handle async calls correctly

--- functions/config/requestOptions.js
+++ functions/config/requestOptions.js
@@ -2,7 +2,7 @@ const keys = require("./keys");
 
 module.exports = {
 	genius: {
-		uri: "https://api.genius.com/search",
+		url: "https://api.genius.com/search",
 		headers: {
 			Authorization: keys.genius
 		},
--- functions/index.js
+++ functions/index.js
@@ -1,73 +1,26 @@
 'use strict';
 
 let options = require('./config/requestOptions');
-const request = require('request');
 
-const {dialogflow} = require('actions-on-google');
-
+const rp = require('request-promise');
+const {dialogflow} = require('actions-on-google')
 const functions = require('firebase-functions');
-
 const app = dialogflow({debug: true});
 
-const fetch = require('node-fetch');
-global.Headers = fetch.Headers;
-
-var myHeaders = new Headers({
-	'Authorization' : 'Bearer U88QBBKT4Q3r46h6EnIgrXyvDt8J_LnYBQvpayFS9js9a1yo9W1NhWh2oHeFDGZS'
-});
-
-// fetch('https://api.genius.com/search?q='+"Eminem", {headers: myHeaders}).then(function(response) {
-// 	response.json().then(function(results) {
-// 		console.log(results.response.hits[0].result.title);
-// 	})
-// })
-
-
-
 app.intent('Default Welcome Intent', (conv) => {
-	conv.ask('Gimme lyrics');
+    conv.ask('Tell me some lyrics');
 });
-
-app.intent('ask for song', (conv, {lyrics}) => {
-	// var songName;
-	// fetch('https://api.genius.com/search?q='+lyrics, {headers: myHeaders}).then(function(response) {
-	// 	response.json().then(function(results) {
-	// 		console.log(results.response.hits[0].result.title);
-	// 		songName = results.response.hits[0].result.title;
-	// 	});
-	// }).then(conv.close('Song is: ' + songName));
-
-	conv.close(lyrics);
-	// conv.close('Song is: ' + songName);
-	// .then(function(callback) {
-	// 	conv.close('Song is'+songName);
-	// });
-});
-
-
-
-// app.intent('Default Welcome Intent', (conv) => {
-// 	conv.ask('Tell me some lyrics');
-// });
 
 app.intent('Default Fallback Intent', (conv) => {
-	options.genius.qs = {q: conv.input.raw}
+    options.genius.qs = {q: conv.input.raw}
 
-	const getSong = () => new Promise((resolve) => {
-		request(options.genius, (err, res, body) => {
-			resolve(body);
-		})
-	});
+    return rp(options.genius).then((body) => {
+        let song = body.response.hits[0].result.title;
+        let artist = body.response.hits[0].result.primary_artist.name;
 
-	return getSong().then((body) => {
-		let song = body.response.hits[0].result.title;
-		let artist = body.response.hits[0].result.primary_artist.name;
-
-		conv.close("The song is " + song + " by " + artist);
-	});
+        conv.close("The song is " + song + " by " + artist);
+        return Promise.resolve();
+    });
 });
 
-
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
-
-
