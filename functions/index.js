83cbd52c322fc4ed75c3d4bf9d56b01d2f8f0b68
wchinny <winnchintip@gmail.com>
2018-09-09 00:28:05 -0500

    current WIP code pushed

--- functions/index.js
+++ functions/index.js
@@ -9,25 +9,63 @@ const functions = require('firebase-functions');
 
 const app = dialogflow({debug: true});
 
-exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
+const fetch = require('node-fetch');
+global.Headers = fetch.Headers;
+
+var myHeaders = new Headers({
+	'Authorization' : 'Bearer U88QBBKT4Q3r46h6EnIgrXyvDt8J_LnYBQvpayFS9js9a1yo9W1NhWh2oHeFDGZS'
+});
+
+// fetch('https://api.genius.com/search?q='+"Eminem", {headers: myHeaders}).then(function(response) {
+// 	response.json().then(function(results) {
+// 		console.log(results.response.hits[0].result.title);
+// 	})
+// })
+
+// app.intent('Default Welcome Intent', (conv) => {
+// 	conv.ask('Tell me some lyrics');
+// });
 
 app.intent('Default Welcome Intent', (conv) => {
-	conv.ask('Tell me some lyrics');
+	conv.ask('Gimme lyrics');
 });
 
-app.intent('Default Fallback Intent', (conv) => {
-	options.genius.qs = {q: conv.input.raw}
+app.intent('ask for song', (conv, {lyrics}) => {
+	// var songName;
+	// fetch('https://api.genius.com/search?q='+lyrics, {headers: myHeaders}).then(function(response) {
+	// 	response.json().then(function(results) {
+	// 		console.log(results.response.hits[0].result.title);
+	// 		songName = results.response.hits[0].result.title;
+	// 	});
+	// }).then(conv.close('Song is: ' + songName));
 
-	const getSong = () => new Promise((resolve) => {
-		request(options.genius, (err, res, body) => {
-			resolve(body);
-		})
-	});
-
-	return getSong().then((body) => {
-		let song = body.response.hits[0].result.title;
-		let artist = body.response.hits[0].result.primary_artist.name;
-
-		conv.close("The song is " + song + " by " + artist);
-	});
+	conv.close(lyrics);
+	// conv.close('Song is: ' + songName);
+	// .then(function(callback) {
+	// 	conv.close('Song is'+songName);
+	// });
 });
+
+
+
+// app.intent('Default Fallback Intent', (conv) => {
+// 	options.genius.qs = {q: conv.input.raw}
+
+// 	const getSong = () => new Promise((resolve) => {
+// 		request(options.genius, (err, res, body) => {
+// 			resolve(body);
+// 		})
+// 	});
+
+// 	return getSong().then((body) => {
+// 		let song = body.response.hits[0].result.title;
+// 		let artist = body.response.hits[0].result.primary_artist.name;
+
+// 		conv.close("The song is " + song + " by " + artist);
+// 	});
+// });
+
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
+
+
