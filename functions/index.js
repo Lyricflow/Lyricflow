64e9e9ab463f5c92c5af8e9e699e495e0f39d6ec
Henry Jetmundsen <henryjetmundsen@gmail.com>
2018-09-14 13:34:45 -0400

    This version of the code now works on dialogflow and the api call is correct. We had to upgrade from basic to blaze on firebase to get the call working

--- functions/index.js
+++ functions/index.js
@@ -11,26 +11,29 @@ const app = dialogflow({debug: true});
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
 
 app.intent('Default Welcome Intent', (conv) => {
-    conv.ask('Give me the lyrics.');
+  conv.ask('Give me the lyrics.');
 });
 
 app.intent('Default Fallback Intent', (conv) => {
-    conv.close('I\'m not having it');
+  conv.close('I\'m not having it');
 })
 
 app.intent('ask for song', (conv, {lyrics}) => {
-    options.genius.qs = {q: lyrics}
+  options.genius.qs = {q: lyrics}
 
-    return new Promise((resolve, reject) => {
-        request(options.genius, (err, res, body) => {
-            if(err){
-                reject(err);
-            } else {
-                let song = body.response.hits[0].result.title;
-                let artist = body.response.hits[0].result.primary_artist.name;
-                let result = conv.close("The song is " + song + " by " + artist);
-                resolve(result);
-            }
-        })
+  const getSong = () => new Promise((resolve, reject) => {
+    request(options.genius, (err, res, body) => {
+      if(err)
+        reject(err);
+      else
+        resolve(body);
     })
+  })
+
+  return getSong().then((body) => {
+    let song = body.response.hits[0].result.title;
+    let artist = body.response.hits[0].result.primary_artist.name;
+    let result = conv.close("The song is " + song + " by " + artist);
+    return result;
+  })
 });
